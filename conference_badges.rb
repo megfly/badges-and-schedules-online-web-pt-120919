801874374d222fe13a8be7aa8a9a51ee11eeb67a
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 00:04:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,15 @@ def batch_badge_creator(array_of_names)
 end 
 
 
-def assign_rooms
+def assign_rooms(speakers)
+  new_array
+  counter = 1 
   
+  speakers.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter +=1 
+  end 
+  return new_array
 end 
 
 def printer
